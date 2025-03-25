8a95b26fc19c2fd80eecbf3f7a89b58884c28b62
Thamarai <tmanalan710@gmail.com>
2025-03-25 11:12:29 +0530

    Title created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Nav } from "./components/Nav";
+import { Title } from "./components/Title";
 const App = ()=>{
 
   return (
     <div>
       <Nav/>
+      <Title/>
     </div>
   );
 }
---
+++ src/components/Title.tsx
@@ -0,0 +1,25 @@
+export const Title = () => {
+    return <>
+
+        <div>
+            <div className="m-6 flex justify-items-center pt-10 ">
+
+                <div className="text-6xl font-mono p-10 font-bold">
+                    Life is either a daring adventure or nothing at all.
+                </div>
+
+            </div>
+            <div className="m-6">
+                <div className="w-[700px] px-10 font-mono">"Welcome to Xplorica – where every journey is an adventure waiting to unfold.
+                    Discover breathtaking destinations, hidden gems, and unforgettable experiences.
+                    Whether you're a traveler, an explorer, or a dreamer, we bring the world closer to you.
+                    Join us as we navigate through cultures, landscapes, and stories that inspire.
+                    Xplorica is more than just a name – it's your passport to exploration!"
+                    </div>
+            </div>
+            <div className="m-6 px-10 py-5">
+                <button className="bg-green-500 p-2 px-4 rounded border-1">Scroll Down To See More</button>
+            </div>
+        </div>
+    </>
+}
